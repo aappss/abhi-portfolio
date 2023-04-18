29f641ca2fd9b84a46c8de062889af14711ec988
Abhishek pratap Singh <abhishekpratapsingh@Abhisheks-MacBook-Pro.local>
2023-04-18 22:09:51 +0530

    design

--- src/component/Header/HeaderFooter.js
+++ src/component/Header/HeaderFooter.js
@@ -1,11 +1,12 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGitHub } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faAlternateGitHub } from '@fortawesome/free-solid-svg-icons';
+// import * as Icons from "@fortawesome/fontawesome-free-solid"
 
 function HeaderFooter() {
     return (
         <div className='header-footer'>
-            <a href="#"><FontAwesomeIcon icon={faGitHub} /></a>
-            <a href="#">linkdIn</a>
+            <a href="#">G</a>
+            <a href="#">L</a>
         </div>
     )
 }
